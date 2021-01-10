d0a03ccf5aae799b3871f2b91ab1e0420802ec91
revyrob <kayle.robson@gmail.com>
2021-01-10 21:52:41 +0100

    init commit

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require('express')
+const path = require('path')
+const PORT = process.env.PORT || 5000
+
+express()
+    .use(express.static(path.join(__dirname, 'public')))
+    .set('views', path.join(__dirname, 'views'))
+    .set('view engine', 'js')
+    .get('/', (req, res) => res.render('pages/index'))
+    .get('/map2', (req, res) => res.render('pages/map2'))
+    //.get('/about', (req, res) => res.render('pages/index2-about'))
+    //.get('/how', (req, res) => res.render('pages/how'))
+    //.get('/guide', (req, res) => res.render('pages/guide'))
+    //.get(/styles/v1/'revyrob'/'mapbox://styles/revyrob/ckavkxl2pdm6f1iqtkary62a9'/wtms)
+    // .get('/map', (req, res) => res.sendFile(path.join(__dirname + '/views/qgis/map.html')))
+
+    .listen(PORT, () => console.log(`Listening on ${PORT}`))
---
+++ partial/nav.js
@@ -0,0 +1,17 @@
+<nav class='navbar navbar-static-top'>
+    <div class='topnav' id='myTopnav'>
+        <ul class='nav'></ul>
+        <li class="active">
+            <a href="/"><span class="glyphicon glyphicon-home"></span> Accueil</a>
+        </li>
+        <li>
+            <a href="/map2"><span class="glyphicon glyphicon-globe"></span> Carte VTT</a>
+        </li>
+        <li>
+            <a href="/about"><span class="glyphicon glyphicon-info-sign"></span> à Propos</a>
+        </li>
+        <li>
+            <a href="/guide"><span class="glyphicon glyphicon-sunglasses"></span> Guide</a>
+        </li>
+    </div>
+</nav>
