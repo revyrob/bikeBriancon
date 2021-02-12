b97ecffdbf784c67c44cabbc19a817347315e3a4
revyrob <kayle.robson@gmail.com>
2021-02-12 14:11:39 +0100

    adding map

---
+++ data/PuyFreyssinetFreyssinet_77.js
@@ -0,0 +1,8 @@
+var json_PuyFreyssinetFreyssinet_77 = {
+"type": "FeatureCollection",
+"name": "PuyFreyssinetFreyssinet_77",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "Nom": "Puy Freyssinet-Freyssinet", "Niveau": "T3", "Longueur": "1.3km", "Alt Max": "1736m", "Source": "www.vttour.fr\/sentiers\/puy-freyssinet-freyssinet,6805.html" }, "geometry": { "type": "MultiLineString", "coordinates": [ [ [ 6.550154341482301, 44.967819866589984 ], [ 6.549596442007219, 44.967789503811922 ], [ 6.548652304434087, 44.968852191473431 ], [ 6.548137320303351, 44.968336031353793 ], [ 6.547450674795586, 44.968244943791767 ], [ 6.545605314993666, 44.967485875149464 ], [ 6.545004500174415, 44.966787523127181 ], [ 6.543674124503141, 44.966726796462773 ], [ 6.542772902274266, 44.967607326807467 ], [ 6.541657103324281, 44.966878613003587 ], [ 6.542129172110848, 44.966423162175076 ], [ 6.540412558341522, 44.965299701336562 ], [ 6.539082182670338, 44.965087152216164 ] ] ] } }
+]
+}
---
+++ data/chain_BrianconauLaSalle_8.js
@@ -0,0 +1,8 @@
+var json_chain_BrianconauLaSalle_8 = {
+"type": "FeatureCollection",
+"name": "chain_BrianconauLaSalle_8",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+
+]
+}
