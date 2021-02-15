f436c4e0c3fbc1ca0341b33fc3ea74795bd69834
revyrob <kayle.robson@gmail.com>
2021-02-15 21:01:40 +0100

    update map data

---
+++ data/BoisduCassetMonetier_5.js
@@ -0,0 +1,8 @@
+var json_BoisduCassetMonetier_5 = {
+"type": "FeatureCollection",
+"name": "BoisduCassetMonetier_5",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "Niveau": "T2", "Nom": "Bois du Casset-Monétier", "Longueur": "0.9km", "Source": "www.vttour.fr\/sentiers\/bois-du-casset-monetier,6057.html", "Alt Max(m)": 1705.0 }, "geometry": { "type": "MultiLineString", "coordinates": [ [ [ 6.484936, 44.97781 ], [ 6.486004499969988, 44.977132482024899 ], [ 6.486648230133496, 44.977344986510595 ], [ 6.486519484100811, 44.977800350613329 ], [ 6.486734060822012, 44.977678920539311 ], [ 6.486862806854696, 44.978073567339194 ], [ 6.487806944427828, 44.977253913256114 ], [ 6.489094404754756, 44.977314628774955 ], [ 6.49038186508168, 44.977800350613329 ], [ 6.491154341277873, 44.979712840355624 ], [ 6.491926817474066, 44.980077116883628 ] ] ] } }
+]
+}
---
+++ data/PuyChirouzan_31.js
@@ -0,0 +1,8 @@
+var json_PuyChirouzan_31 = {
+"type": "FeatureCollection",
+"name": "PuyChirouzan_31",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+
+]
+}
