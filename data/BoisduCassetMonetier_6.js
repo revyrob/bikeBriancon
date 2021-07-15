7f8f53771d0a97b68028e4806fafbb623264df13
unknown <kayle.robson@gmail.com>
2021-07-15 21:10:46 +0200

    update

--- data/BoisduCassetMonetier_6.js
+++
@@ -1,8 +0,0 @@
-var json_BoisduCassetMonetier_6 = {
-"type": "FeatureCollection",
-"name": "BoisduCassetMonetier_6",
-"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
-"features": [
-{ "type": "Feature", "properties": { "Nom": "Bois du Casset-Monétier", "Niveau": "T2", "Longueur": "0.9km", "Alt Max": "1705m", "Source": "www.vttour.fr\/sentiers\/bois-du-casset-monetier,6057.html" }, "geometry": { "type": "MultiLineString", "coordinates": [ [ [ 6.484936, 44.97781 ], [ 6.486004499969988, 44.977132482024899 ], [ 6.486648230133496, 44.977344986510595 ], [ 6.486519484100811, 44.977800350613329 ], [ 6.486734060822012, 44.977678920539311 ], [ 6.486862806854696, 44.978073567339194 ], [ 6.487806944427828, 44.977253913256114 ], [ 6.489094404754756, 44.977314628774955 ], [ 6.49038186508168, 44.977800350613329 ], [ 6.491154341277873, 44.979712840355624 ], [ 6.491926817474066, 44.980077116883628 ] ] ] } }
-]
-}
--- data/chain_BrianconauLaSalle_8.js
+++
@@ -1,8 +0,0 @@
-var json_chain_BrianconauLaSalle_8 = {
-"type": "FeatureCollection",
-"name": "chain_BrianconauLaSalle_8",
-"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
-"features": [
-
-]
-}
